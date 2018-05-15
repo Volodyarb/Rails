e980dee50903f16bb001db94f704925df42ae6f1
volodya <996504@i.ua>
2018-05-15 15:46:05 +0300

    Rails

---
+++ db/migrate/20180515092756_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.integer :user_id
+      t.string :name
+      t.decimal :rental_price
+      t.integer :city_id
+
+      t.timestamps
+    end
+  end
+end
